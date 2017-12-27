e1dbda1ba0b08e1cf2eb39d1082fbc6d7f2c51cc
Mariana Rodrigues <marianarodrigues@MacBook-Air-de-Mariana.local>
2017-12-27 01:20:35 +0000

    js adds

---
+++ js/client.js
@@ -0,0 +1,38 @@
+$(window).ready(
+    function() {
+
+        createExperiences();
+
+    }
+);
+
+function createExperiences() {
+
+ $.getJSON('http://localhost/projetoACR/ACR%20Code/mockdata/experienceDetails.json',
+    function(data){
+        var experienceTitle = $("#experienceDetailsHeader");
+        experienceTitle.append("<h1>" + data.header + "</h1>");
+        $("#experienceAtributes").append("<ul class='atributesList fa-ul fa-2x'>"+
+        "<li><i class='fa-li fa fa-check-square'></i>"+ data.Atributs.location + "</li>"+
+        "<li><i class='fa-li fa fa-check-square'></i>"+data.Atributs.level+"</li>"+
+        "<li><i class='fa-li fa fa-check-square'></i>"+data.Atributs.category+"</li>"+
+        "<li><i class='fa-li fa fa-check-square'></i>"+data.Atributs.duration+"</li>"+
+        "</ul>");
+
+        var carousel = $('.carouselItems');
+
+        carousel.append("<div> <img src='"+ data.image.cover +"'></img></div>");
+
+        $(data.image.others).each(function(i, item){
+            carousel.append("<div> <img src='"+ data.image.others[i].image +"'></img></div>");
+        });
+
+        carousel.slick({
+           dots: true,
+           arrows: true
+         });
+
+    });
+
+
+}
