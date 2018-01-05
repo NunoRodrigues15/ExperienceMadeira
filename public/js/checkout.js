2c46fd87806852920c83d02caae5df80973c45d4
JPBorges <joao.borges@factory.odkas.com>
2018-01-05 19:03:50 +0000

    amend

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -143,7 +143,7 @@ function redirect() {
 
 
 function updatePrice() {
-    $j("#h3priceid").html = pricePerPerson * ($j("#quantity").val()) + " €"
+    h3priceid.innerText = pricePerPerson * ($j("#quantity").val()) + " €";
 }
 
 
