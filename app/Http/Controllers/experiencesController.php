5c7197ff2d80b32cf11f01d08f1565c70ce2adfb
JPBorges <joao.borges@factory.odkas.com>
2018-01-05 13:45:33 +0000

    amed

--- app/Http/Controllers/experiencesController.php
+++ app/Http/Controllers/experiencesController.php
@@ -17,34 +17,10 @@ class experiencesController extends Controller
     public function index()
     {
         $experienceResult = DB::table('experiences')
-            ->leftJoin('companies', 'experiences.company_id', '=', 'companies.id')
-            ->leftJoin('categories', 'experiences.categories_id', '=', 'categories.id')
-            ->leftJoin('locations', 'experiences.locations_id', '=', 'locations.id')
-            ->leftJoin('requirements', 'experiences.requirements_id', '=', 'requirements.id')
             ->leftJoin('experience_images', 'experiences.experience_images_id', '=', 'experience_images.id')
             ->select('experiences.name',
                     'experiences.description',
-                    'experiences.short_description',
-                    'experiences.start_date',
-                    'experiences.end_date',
-                    'experiences.weekend_open',
-                    'experiences.level',
-                    'experiences.duration',
-                    'experiences.price',
-                    'experiences.price_unit',
-                    'experiences.max_people',
-                    'companies.name AS companies_name',
-                    'categories.name AS categories_name',
-                    'experience_images.cover AS experience_images_cover',
-                    'experience_images.img1 AS experience_images_img1',
-                    'experience_images.img2 AS experience_images_img2',
-                    'locations.name AS locations_name',
-                    'locations.coord_x AS locations_coord_x',
-                    'locations.coord_y AS locations_coord_y',
-                    'locations.typical_weather AS locations_typical_weather',
-                    'requirements.equipment AS requirements_equipment',
-                    'requirements.age AS requirements_age',
-                    'requirements.other AS requirements_other'
+                    'experience_images.cover AS experience_images_cover'
             )
             ->get();
 
@@ -110,7 +86,7 @@ class experiencesController extends Controller
                     'requirements.age AS requirements_age',
                     'requirements.other AS requirements_other'
             )
-            ->where('id', '=', $experience->id)
+            ->where('experiences.id', '=', $experience->id)
             ->get();
 
         return response($experienceResult, 200);
