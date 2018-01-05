449ebb28bfb7405ce39f7dd7f48841e4f9e710c8
JPBorges <joao.borges@factory.odkas.com>
2018-01-05 14:41:00 +0000

    maps

--- public/js/detail.js
+++ public/js/detail.js
@@ -22,7 +22,7 @@ function createExperiences() {
             $("#experienceAtributes").append("<ul class='atributesList fa-ul'>" +
                 "<li><i class='fa-li fa fa-map-signs'></i>" + data.locations_name + "</li>" +
                 "<li><i class='fa-li fa fa-line-chart'></i>" + data.level + "</li>" +
-                "<li><i class='fa-li fa fa-check-square'></i>" + data.categories_name+ "</li>" +
+                "<li><i class='fa-li fa fa-check-square'></i>" + data.categories_name + "</li>" +
                 "<li><i class='fa-li fa fa-clock-o'></i>" + data.duration + "</li>" +
                 "</ul>");
 
@@ -35,27 +35,29 @@ function createExperiences() {
                 "<li><i class='fa-li fa fa-user-o'></i>" + data.requirements_age + "</li>" +
                 "</ul>");
 
+            $("#priceSection").append("<div class='price'>" + "<p>" + data.price + data.price_unit + " por pessoa</p>" + "<//div>");
+            $("#priceSection").append("<div class='dateButton'> " + "<button class='button buttonPos' onclick=navigatetoCheckout('" + window.location.search.substring(1) + "')>Ver datas</button>" + "</div");
 
+            var carousel = $('.carouselItems');
 
-        $("#priceSection").append("<div class='price'>" + "<p>" + data.price + data.price_unit + " por pessoa</p>" + "<//div>");
-        $("#priceSection").append("<div class='dateButton'> " + "<button class='button buttonPos' onclick=navigatetoCheckout('"+window.location.search.substring(1)+"')>Ver datas</button>" + "</div");
+            carousel.append("<div>" + " <img src='" + data.experience_images_cover + "'></img>" + "</div>");
+            carousel.append("<div>" + " <img src='" + data.experience_images_img1 + "'></img>" + "</div>");
+            carousel.append("<div>" + " <img src='" + data.experience_images_img2 + "'></img>" + "</div>");
 
-        var carousel = $('.carouselItems');
+            myMap(data.locations_coord_x, data.locations_coord_y);
 
-        carousel.append("<div>" + " <img src='" + data.experience_images_cover + "'></img>" + "</div>");
-        carousel.append("<div>" + " <img src='" + data.experience_images_img1 + "'></img>" + "</div>");
-        carousel.append("<div>" + " <img src='" + data.experience_images_img2 + "'></img>" + "</div>");
-
-        carousel.slick({
-            slidesToShow: 1,
-            dots: true,
-            arrows: true
         });
-
-        $(".fb-comments").attr("data-href", window.location.href); $(".fb-comments").attr("data-width", $(".col-right").width());
+    carousel.slick({
+        slidesToShow: 1,
+        dots: true,
+        arrows: true
     });
 
+    $(".fb-comments").attr("data-href", window.location.href);
+    $(".fb-comments").attr("data-width", $(".col-right").width());
+
 }
+
 function navigatetoCheckout(id) {
     var pathname = window.location.pathname;
     var splitPath = pathname.split("/");
@@ -64,6 +66,16 @@ function navigatetoCheckout(id) {
     path = path + "checkout.html?" + id;
     window.location.href = path;
 }
+
 function isNumeric(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function myMap(x, y) {
+    var mapOptions = {
+        center: new google.maps.LatLng(x, y),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
